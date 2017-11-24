9c00d32a09f5320d7a9b4dc32edfcbbe244f18e3
Smrity chaudhary <smrity.mca17.du@gmail.com>
2017-11-24 23:10:19 +0530

    append 'ing' at the end of user entered strings

---
+++ append ing.py
@@ -0,0 +1,28 @@
+def check(string):
+    length = len(string)
+    if length >= 3:
+        string = append(string)
+    return string
+
+def append(string):
+    if string[3:] == 'ing':
+        string = string + 'ly'
+        return string
+    else:
+        string = string + 'ing'
+        return string
+
+def main():
+    '''
+     Objective: Take input from user
+     Input values:
+         number: The input number as list
+     Output value: Print whether the input is evil or not
+     '''
+    #approach:
+    string = input('Enter the string: ') 
+    string = check(string)
+    print('New string is: ' ,string)
+
+if (__name__ == '__main__'):
+    main()
