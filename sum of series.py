62c0dc6648b8c415c5cf569f40c8e6bc9ec6f4af
Smrity chaudhary <smrity.mca17.du@gmail.com>
2017-12-13 18:14:00 +0530

    doumentation

--- sum of series.py
+++ sum of series.py
@@ -1,12 +1,25 @@
 def compute(number,count):
+    '''
+    Objective: To compute the sum of series n+nn+nnn....
+    Input Parameter: 
+            number: The number whose series need to find
+            count: number of elements in the series
+    Return value: sum of the series
+    '''
     sum = 0
     result = 0
     for i in range(0,count):
-        sum = number*pow(10,i)+sum
+        sum = number * pow(10,i)+sum
         result = result + sum
         
     return result
+
 def main():
+    '''
+    Objective: Call a function to compute the sum of series n+nn+nnn....
+    Input Parameter: None
+    Return value: None
+    '''
     number = int(input("Enter the number: "))
     count = int(input("Enter the count: "))
     x = compute(number,count)
