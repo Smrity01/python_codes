b948da924c157eabcf0731c4ce57180741ce1aa5
Smrity chaudhary <smrity.mca17.du@gmail.com>
2018-01-13 20:48:47 +0530

    documentation

--- sum of elements(nested list).py
+++ sum of elements(nested list).py
@@ -1,4 +1,11 @@
 def calculate(mylist):
+        '''
+    Objective       : To Calculate the sum of nested list
+    Input Parameter :
+            mylist  : Nested List of elements
+    Return value    : Return sum of elements of the list
+    
+    '''
     sum = 0
     for pos in range(0,len(mylist)):
         if (type(mylist[pos]) == list):
@@ -7,6 +14,12 @@ def calculate(mylist):
             sum = sum + mylist[pos]
     return sum
 def main():
+    '''
+    Objective       : For Calling the function 
+    Input Parameter : None
+    Return value    : None
+    
+    '''
     mylist = [2,1,[5,6],3,[[1]]]
     sum = calculate(mylist)
     print('Sum of elements of the list: ',sum)
