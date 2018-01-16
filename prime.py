4b2292d4957521acf9aab62ed6c5e9f1ed099667
Smrity chaudhary <smrity.mca17.du@gmail.com>
2018-01-16 23:17:27 +0530

    fermat's little theorem

---
+++ prime.py
@@ -0,0 +1,18 @@
+def check_prime(number):
+    '''
+    Objective       : To check whether the given number is prime or not
+    Input parameter : User entered number
+    Return value    : Boolean value true or false
+    '''
+    power = pow(2,number-1)
+    if (power % number == 1):
+        return True
+    else:
+        return False
+
+num = int(input('Enter a number: '))
+print("****USING FERMAT'S LITTLE THEOREM******")
+if (check_prime(num) == True):
+    print('Its is a prime...')
+else: print('Its is NOT a prime...')
+    
